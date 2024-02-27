5940c58a6f3e0d10d09aa3f72fe3c80d94f6e46f
Dieggo007 <100090107+Dieggo007@users.noreply.github.com>
2024-02-27 16:06:05 -0500

    add the entire project

---
+++ caesar_cipher.rb
@@ -0,0 +1,17 @@
+def letter?(char)
+  char.match?(/[A-Za-z]/)
+end
+
+def is_upper?(char)
+  char == char.upcase
+end
+
+def shift_char(char, shift)
+  limits = is_upper?(char) ? {upper: 90, lower: 64} : {upper: 122, lower: 96}
+  char.ord + shift <= limits[:upper] ? (char.ord + shift).chr : (char.ord + shift + limits[:lower] - limits[:upper]).chr
+end
+
+def caesar_cipher(string, shift)
+  string.chars.map{ |char| letter?(char) ? shift_char(char, shift) : char }.join
+end
+
