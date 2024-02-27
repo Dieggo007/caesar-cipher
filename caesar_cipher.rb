d4c641735abf70d945b7907b5662d3ce1c994441
Dieggo007 <100090107+Dieggo007@users.noreply.github.com>
2024-02-27 16:26:35 -0500

    Update caesar_cipher.rb

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -8,7 +8,9 @@ end
 
 def shift_char(char, shift)
   limits = is_upper?(char) ? {upper: 90, lower: 64} : {upper: 122, lower: 96}
-  char.ord + shift <= limits[:upper] ? (char.ord + shift).chr : (char.ord + shift + limits[:lower] - limits[:upper]).chr
+  char.ord + shift <= limits[:upper] 
+    ? (char.ord + shift).chr 
+    : (char.ord + shift + limits[:lower] - limits[:upper]).chr
 end
 
 def caesar_cipher(string, shift)
